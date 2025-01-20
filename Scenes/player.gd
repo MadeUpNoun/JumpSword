cb683be8b84e2a3a341fdc7c96fb161d5ae9cc60
MadeUpNoun <141415330+MadeUpNoun@users.noreply.github.com>
2025-01-20 22:14:49 +1100

    patched sword movement
    made the controls hold instead of press as well as clamped movement and made it slow down when buttons stop being pressed.
    also added animated arrow

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,26 +1,31 @@
 extends RigidBody2D
 
 @onready var Arrow_turn_table = $Arrow_turn_table
-
+var turn_direction = 0.0
 # not entirely sure but this func is specifcally whats used to move rigid bodys with forces
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	var velocity := state.get_linear_velocity()
-	pass
+	if Input.is_action_just_released("Leap"):
+		pass
 
-
+#phys func that runs each frame
 func _physics_process(delta: float) -> void:
 	#controls left and right turning
-	var turn_direction = 0
-	if Input.is_action_just_pressed("Move_Left"):
-		turn_direction += -10.0
-	if Input.is_action_just_pressed("Move_Right"):
-		turn_direction += 10.0
+	if Input.is_action_pressed("Move_Left"):
+		turn_direction = clamp(turn_direction - 0.01,-0.5,0.5)
+	elif Input.is_action_pressed("Move_Right"):
+		turn_direction = clamp(turn_direction + 0.01,-0.5,0.5)
+	else:
+		if turn_direction > 0:
+			turn_direction -= 0.1
+		else:
+			turn_direction += 0.1
+			print(turn_direction)
 	angular_velocity += turn_direction
 	
-	Arrow_turn_table.look_at(get_global_mouse_position())
-	
-	if InputEventMouseButton.MOUSE_BUTTON_LEFT.is_pressed(): 
+	if Input.is_action_pressed("Leap"):
 		Arrow_turn_table.show()
 	else:
 		Arrow_turn_table.hide()
+	
+	Arrow_turn_table.look_at(get_global_mouse_position())
 	
