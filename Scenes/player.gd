ea056602fd0d5de69032071978434f0d2ce6a0a2
MadeUpNoun <141415330+MadeUpNoun@users.noreply.github.com>
2025-01-20 12:06:44 +1100

    added leap arrow
    added arrow that turns to follow the mouse

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,17 +1,26 @@
 extends RigidBody2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var Arrow_turn_table = $Arrow_turn_table
+
+# not entirely sure but this func is specifcally whats used to move rigid bodys with forces
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var velocity := state.get_linear_velocity()
 	pass
 
 
 func _physics_process(delta: float) -> void:
+	#controls left and right turning
 	var turn_direction = 0
 	if Input.is_action_just_pressed("Move_Left"):
 		turn_direction += -10.0
 	if Input.is_action_just_pressed("Move_Right"):
 		turn_direction += 10.0
 	angular_velocity += turn_direction
-	print (angular_velocity)
+	
+	Arrow_turn_table.look_at(get_global_mouse_position())
+	
+	if InputEventMouseButton.MOUSE_BUTTON_LEFT.is_pressed(): 
+		Arrow_turn_table.show()
+	else:
+		Arrow_turn_table.hide()
+	
