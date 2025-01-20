70fb107b4c6959f8a58555a0d874879c867e3403
AR <156492411+A-RIZ0@users.noreply.github.com>
2025-01-21 01:14:23 +1100

    Created temporary menu

---
+++ Scenes/menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_pressed():
+	# Change To Game Scene When Created
+	get_tree().change_scene_to_file("res://Scenes/test_world.tscn")
+	# Change When Settings implemented
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
