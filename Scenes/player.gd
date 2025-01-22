d8aeb9cd1ddb69a1e14ff56120f1005120a49c9b
MadeUpNoun <141415330+MadeUpNoun@users.noreply.github.com>
2025-01-22 12:02:13 +1100

    added on_floor check
    rigid bodies do not have an inbuilt is on floor func so i made one using an area2D that checks for collisions with collsion mask level 2s

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -5,12 +5,21 @@ extends RigidBody2D
 var turn_direction = 0.0
 
 var leap_cooldown = false
-
+var on_floor = false
 # not entirely sure but this func is specifcally whats used to 
 #move rigid bodys with forces
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if Input.is_action_just_released("Leap"):
-		pass
+#func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+#
+	#var i := 0
+	#while i < state.get_contact_count():
+		#var normal := state.get_contact_local_normal(i)
+		#if normal.dot(Vector2.UP) != null:
+			#on_floor = normal.dot(Vector2.UP) >  0.99
+		#else:
+			#on_floor = false
+		#i += 1
+
+
 
 #phys func that runs each frame
 func _physics_process(delta: float) -> void:
@@ -28,7 +37,7 @@ func _physics_process(delta: float) -> void:
 	
 	Arrow_turn_table.look_at(get_global_mouse_position())
 	if leap_cooldown == false:
-		if Input.is_action_pressed("Leap"):
+		if Input.is_action_pressed("Leap") and on_floor:
 			Arrow_turn_table.show()
 			leap_charge()
 		elif Input.is_action_just_released("Leap"):
@@ -39,6 +48,11 @@ func _physics_process(delta: float) -> void:
 			charge = 0.0
 			$Leap_cooldown.start()
 			leap_cooldown = true
+		else:
+			Arrow_turn_table.hide()
+			charge = 0.0
+	
+
 
 
 	
@@ -54,3 +68,15 @@ func leap_charge():
 
 func _on_leap_cooldown_timeout() -> void:
 	leap_cooldown = false
+
+
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	on_floor = true
+	print("entered")
+
+
+func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	on_floor = false
+	print("exited")
